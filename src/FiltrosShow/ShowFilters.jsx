400c35ecde5e31fe40144a85b1b45d230fe088f7
Prince Moreno <prins_ghost_2018@outlook.com>
2024-01-08 14:57:48 -0500

    Agregando filtros

--- src/FiltrosShow/ShowFilters.jsx
+++ src/FiltrosShow/ShowFilters.jsx
@@ -6,12 +6,11 @@ import Filtros from "../handlFitler/Filtros";
 function ShowFilters() {
   const { filterDatos, getRuc, data, datosRuc } = useContext(ApiContext);
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex gap-4 pt-4 items-center">
+    <div className="flex items-center justify-center relative mt-4">
+      <div className="flex flex-1 gap-4 items-center">
        <Filtros/>
       </div>
-
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-center h-full">
         <ExportarPdf
           filterGa={filterDatos}
           getRuc={getRuc}
--- src/handlFitler/Filtros.jsx
+++ src/handlFitler/Filtros.jsx
@@ -13,9 +13,16 @@ export default function Filtros() {
       const tipoMatch = tipoFiltro === "todos" || value.codComp === tipoFiltro;
       console.log(data);
       if (fechaFiltro !== "") {
-        const fechaFiltroFormatted = fechaFiltro.split('/').reverse().join('-');
-        const fechaValueFormatted = value.fechaEmision.split('/').reverse().join('-');
-        return estadoMatch && tipoMatch && fechaValueFormatted === fechaFiltroFormatted;
+        const fechaFiltroFormatted = fechaFiltro.split("/").reverse().join("-");
+        const fechaValueFormatted = value.fechaEmision
+          .split("/")
+          .reverse()
+          .join("-");
+        return (
+          estadoMatch &&
+          tipoMatch &&
+          fechaValueFormatted === fechaFiltroFormatted
+        );
       }
 
       return estadoMatch && tipoMatch;
@@ -39,41 +46,50 @@ export default function Filtros() {
   }; // Add handleFechaFilter
 
   return (
-    <>
-      <p className="">Filtrar por Estado: </p>
-      <select
-        id="filtros"
-        onChange={handleEstadoFilter}
-        className="px-2 py-2 cursor-pointer border text-center"
-      >
-        <option value="todos">Todos</option>
-        <option value="0">No existe</option>
-        <option value="1">Aceptado</option>
-        <option value="2">Anulado</option>
-        <option value="3">Autorizado</option>
-        <option value="4">No autorizado</option>
-      </select>
-      <p>Filtrar por Comprobante:</p>
-      <select
-        id="filtrosComprobante"
-        onChange={handleTipoFilter}
-        className="px-2 py-2 cursor-pointer border text-center"
-      >
-        <option value="todos">Todos</option>
-        <option value="01">Factura</option>
-        <option value="03">Boleta</option>
-        <option value="04">Liquidacion de compra</option>
-        <option value="07">Nota de Credito</option>
-        <option value="08">Nota de Debito</option>
-        <option value="R1">Recibo por honorarios</option>
-        <option value="R7">Nota de credito de recibos</option>
-      </select>
-      <p>Filtrar por Fecha:</p> {/* Add filter by fecha */}
-      <input
-        type="date"
-        onChange={handleFechaFilter}
-        className="px-2 py-2 cursor-pointer border text-center"
-      />
-    </>
+    <div className="w-full">
+      <p className="text-center font-bold absolute left-0 -top-8 right-0">FILTROS</p>
+      <div className="flex justify-around gap-4">
+        <span className="flex items-center">
+          <p>ESTADO: </p>
+          <select
+            id="filtros"
+            onChange={handleEstadoFilter}
+            className="px-2 py-2 cursor-pointer border text-center"
+          >
+            <option value="todos">Todos</option>
+            <option value="0">No existe</option>
+            <option value="1">Aceptado</option>
+            <option value="2">Anulado</option>
+            <option value="3">Autorizado</option>
+            <option value="4">No autorizado</option>
+          </select>
+        </span>
+        <span className="flex items-center">
+          <p>COMPROBANTE: </p>
+          <select
+            id="filtrosComprobante"
+            onChange={handleTipoFilter}
+            className="px-2 py-2 cursor-pointer border text-center"
+          >
+            <option value="todos">Todos</option>
+            <option value="01">Factura</option>
+            <option value="03">Boleta</option>
+            <option value="04">Liquidacion de compra</option>
+            <option value="07">Nota de Credito</option>
+            <option value="08">Nota de Debito</option>
+            <option value="R1">Recibo por honorarios</option>
+            <option value="R7">Nota de credito de recibos</option>
+          </select>
+        </span>
+        <span className="flex items-center">
+          <p>FECHA:</p> {/* Add filter by fecha */}
+          <input
+            type="date"
+            onChange={handleFechaFilter}
+            className="px-2 py-2 cursor-pointer border text-center"
+          />
+        </span>
+      </div>
+    </div>
   );
 }
